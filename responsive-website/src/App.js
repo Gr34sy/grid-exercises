78d5e4396880a2d5697cb4e7356143f58688cf8a
Gr34sy <slupek3@poczta.fm>
2024-01-04 13:06:18 +0100

    initialized responsive-website files

---
+++ responsive-website/src/App.js
@@ -0,0 +1,14 @@
+import { Wrapper } from './components/Wrapper';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        dupa
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
---
+++ responsive-website/src/components/Wrapper.jsx
@@ -0,0 +1,8 @@
+
+export function Wrapper(props){
+    return(
+        <div className="wrapper">
+            {props.children}
+        </div>
+    )
+}
